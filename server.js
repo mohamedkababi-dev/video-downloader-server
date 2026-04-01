b57a5fcb3787a89cab74145e29b9fdbf228e8dd7
mohamedkababi-dev <mohamedkababi660@gmail.com>
2026-04-01 02:34:33 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require("express");
+const ytdl = require("ytdl-core");
+
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Server is working 🚀");
+  res.send("Video Downloader API 🚀");
+});
+
+// تحميل فيديو
+app.get("/download", async (req, res) => {
+  const url = req.query.url;
+
+  if (!url) {
+    return res.send("ضع رابط الفيديو");
+  }
+
+  try {
+    res.header("Content-Disposition", 'attachment; filename="video.mp4"');
+    ytdl(url, { format: "mp4" }).pipe(res);
+  } catch (err) {
+    res.send("خطأ في التحميل");
+  }
 });
 
 const PORT = process.env.PORT || 3000;
